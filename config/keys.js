a2efa420a1b7a828f0f5350f39f2809ddf2a2153
Muhammad Ali <muhammad797@hotmail.com>
2018-12-01 17:52:42 +0500

    Initial Push and Items API basic ready

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://geekster:passw0rd@ds257241.mlab.com:57241/mern_shopping'
+}
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const items = require('./routes/api/items');
+
+const app = express();
+
+// Bodyparser Middleware
+app.use(bodyParser.json());
+
+// MongoDB URI
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+// Use routes
+app.use('/api/items', items);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.warn(`Server started on ${port}`));
